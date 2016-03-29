cf89ae409ebf0255d5ccc983a34410b82bcad2ec
oriken111 <oriken111@gmail.com>
2016-03-29 18:52:10 +0900

    Add rails pipeline configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
+  config.serve_static_assets = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
