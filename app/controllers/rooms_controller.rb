0b6b0bbbdf1d8f9fd2a3f8dd1d43b808800bdc4c
oriken111 <oriken111@gmail.com>
2016-03-29 20:48:34 +0900

    debug rooms_params

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -62,6 +62,6 @@ class RoomsController < ApplicationController
     end
 
     def room_params
-      params.require(:room).permit(:home_type, :room_type,:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_heating, :is_internet, :is_air, :price, :active)
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_heating, :is_internet, :is_air, :price, :active)
     end
 end
