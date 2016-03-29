5dbd54a3d2692aa3a355c40607d669dcd9efdb0b
oriken111 <oriken111@gmail.com>
2016-03-29 18:45:49 +0900

    Add rails pipeline configuration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
-** //= require jquery_ujs **
+//= require jquery_ujs
 //= require jquery-ui/datepicker
 //= require toastr
 //= require private_pub
